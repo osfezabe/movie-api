2c233794daade7c0d17bd4c8caf76a84ac21d70a
Oscar Zapata <oscarfernandozapata@hotmail.com>
2022-12-07 17:25:09 -0500

    Commit inicial con la estructura básica del proyecto

---
+++ src/test/java/co/talataa/movieapi/MovieApiApplicationTest.java
@@ -0,0 +1,13 @@
+package co.talataa.movieapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieApiApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
